53df8fa35d383f270ab1f31d3455405a2f972ac5
Kaloyan Raev <kaloyan-raev@users.noreply.github.com>
2017-04-04 08:47:28 +0300

    Поддръжка на 3G на Kindle Keyboard (#9)
    * Поддръжка на 3G на Kindle Keyboard
    * Добавяне на нужните разрешения за 3G връзка в external.policy

--- src/main/java/name/raev/kaloyan/kindle/chitanka/screen/SplashScreen.java
+++ src/main/java/name/raev/kaloyan/kindle/chitanka/screen/SplashScreen.java
@@ -31,7 +31,7 @@ public class SplashScreen extends Screen {
 	}
 
 	protected int getPageSize() {
-		return 0;
+		return 1;
 	}
 
 	protected void createContent(Container container) throws IOException {
